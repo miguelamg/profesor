9bf19668fcb2af3094461b9e71c971b12c1aa06e
Experis Academy <miguelamg@users.noreply.github.com>
2021-09-19 21:30:05 +0200

    Update Ejecuta.java

--- BaseProgramacion_Ej4/src/paquete/Ejecuta.java
+++ BaseProgramacion_Ej4/src/paquete/Ejecuta.java
@@ -7,24 +7,10 @@ public class Ejecuta
 	public static void main(String[] args) 
 	{
 		/*
-		 4. Programa que muestra todos los divisores de un número introducido. 
-		    Entre los divisores no se incluirá el propio número.
+		 4. Programa que muestra todos los divisores de un nÃºmero introducido. 
+		    Entre los divisores no se incluirÃ¡ el propio nÃºmero.
 		*/
 
-		Scanner sc = new Scanner(System.in);
-		int numero;
 		
-		System.out.println("Introduzca número:");
-		numero = sc.nextInt();
-
-		//for(int candidatoDivisor=1; candidatoDivisor<=numero/2; candidatoDivisor++) {
-		for(int candidatoDivisor=1; candidatoDivisor<numero; candidatoDivisor++) {
-			if( numero%candidatoDivisor == 0) 
-			{
-				System.out.println(candidatoDivisor);
-			}
-		}
-		
-		sc.close();
 	}
 }
