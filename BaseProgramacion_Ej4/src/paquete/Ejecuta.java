a7dad73232113a4f78d275f6b32ed5565837cb37
Miguel <noreply@campus.manpoweracademy.online>
2021-09-19 21:58:44 +0200

    OTRO COMMIT...

--- BaseProgramacion_Ej4/src/paquete/Ejecuta.java
+++ BaseProgramacion_Ej4/src/paquete/Ejecuta.java
@@ -2,15 +2,48 @@ package paquete;
 
 import java.util.Scanner;
 
-public class Ejecuta 
-{
+public class Ejecuta {
+
 	public static void main(String[] args) 
 	{
 		/*
-		 4. Programa que muestra todos los divisores de un nÃºmero introducido. 
-		    Entre los divisores no se incluirÃ¡ el propio nÃºmero.
-		*/
+		    3. Programa que lee de manera consecutiva números hasta que se introducen dos números
+			iguales seguidos.
+		 */
 
-		// bla bla
+		// Declaramos las variables
+		Scanner sc = new Scanner(System.in);
+		int numero;
+		int otroNumero;
+		boolean continuar = true;
+			
+		// Pedimos el primer número antes del bucle
+		System.out.println("Introduzca número");
+		numero = sc.nextInt();
+		
+		do 
+		{
+			System.out.println("Introduzca número");
+			otroNumero = sc.nextInt();
+
+			// Si son iguales no continuamos
+			if(numero==otroNumero) 
+			{
+				continuar = false;
+			}
+			// ...si son distintos
+			else
+			{
+				// Pasamos el valor del último número introducido a la variable numero, 
+				// para dejar libre la variable otroNumero y que pueda recoger el próximo valor 
+				// en la siguiente iteración del bucle.
+				numero = otroNumero;
+			}
+		}while( continuar==true );
+		
+		System.out.println("Fin");
+		
+		sc.close();
 	}
+
 }
